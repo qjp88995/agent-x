b720fc62715dc9c582fefdcfa8decdb80338a2c1
qinjiapeng <759848795@qq.com>
2026-03-06 20:57:41 +0800

    chore: add react-hook-form, zod, and shadcn Form component
    Install react-hook-form, @hookform/resolvers, and zod for form
    management. Generate shadcn/ui Form component with FormField,
    FormItem, FormLabel, FormControl, FormDescription, FormMessage.

--- packages/web/src/components/ui/label.tsx
+++ packages/web/src/components/ui/label.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import * as LabelPrimitive from '@radix-ui/react-label';
+import { Label as LabelPrimitive } from 'radix-ui';
 
 import { cn } from '@/lib/utils';
 
